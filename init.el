59e9e75987bd26ab5095f4fcfc9a45681ebb4775
crolbar <crolbar@crolbar.com>
2024-11-25 22:54:38 +0200

    batman (the beginning)

---
+++ init.el
@@ -0,0 +1,31 @@
+(load-theme 'tsdh-dark t)
+
+(set-face-attribute 'default nil :font "Hack" :height 150)
+
+; line wrap off
+(setq-default truncate-lines t)
+
+; harpoon muscle memory
+(global-set-key (kbd "M-a") (lambda () (interactive) (find-file "~/.emacs.d/init.el")))
+(global-set-key (kbd "M-o") (lambda () (interactive) (find-file "~/emacs-note.md")))
+(global-set-key (kbd "M-e") (lambda () (interactive) (switch-to-buffer "TUTORIAL")))
+
+; rebind M-t because it looks usefull
+(global-set-key (kbd "M-y") 'transpose-words)
+
+
+
+(use-package magit :ensure t)
+
+
+(use-package evil :ensure t)
+
+(evil-mode 8)
+
+; C-r for redo
+(evil-set-undo-system 'undo-redo)
+
+; C-u for half-screen up
+(with-eval-after-load 'evil
+  (define-key evil-normal-state-map (kbd "C-u") 'evil-scroll-up)
+  (define-key evil-visual-state-map (kbd "C-u") 'evil-scroll-up))
